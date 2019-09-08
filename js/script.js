f3b98a7732738b10cae7e2ec6772823d45afe765
Kajetan Terlecki <k.terlecki@wp.pl>
2019-09-08 17:33:03 +0200

    Add getting 'href' from clicked link

--- js/script.js
+++ js/script.js
@@ -7,6 +7,7 @@
 const titleClickHandler = function () {
     console.log('Link was clicked!');
     console.log(event);
+    
 
     /* [DONE] remove class 'active' from all article links  */
 
@@ -16,9 +17,10 @@ const titleClickHandler = function () {
         activeLink.classList.remove('active');
     }
 
-    /* [IN PROGRESS] add class 'active' to the clicked link */
+    /* [DONE] add class 'active' to the clicked link */
+    event.preventDefault();
 
-    const clickedElement = this 
+    const clickedElement = this;
         clickedElement.classList.add('active');
 
     console.log('clickedElement:', clickedElement);
@@ -31,9 +33,13 @@ const titleClickHandler = function () {
         activeArticle.classList.remove('active');
     }
 
-    /* get 'href' attribute from the clicked link */
+    /* [DONE] get 'href' attribute from the clicked link */
 
-    /* find the correct article using the selector (value of 'href' attribute) */
+    const articleSelector = clickedElement.getAttribute("href");
+    
+    console.log(articleSelector)
+
+    /* [IN PROGRESS] find the correct article using the selector (value of 'href' attribute) */
 
     /* add class 'active' to the correct article */
 }
