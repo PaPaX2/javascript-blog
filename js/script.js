632913389902ecc823376fae2a84005f057c83e7
Kajetan Terlecki <k.terlecki@wp.pl>
2019-09-10 10:51:04 +0200

    ADD loop for finding Tags in Articles

--- js/script.js
+++ js/script.js
@@ -140,32 +140,44 @@
     for (let link of links) {
         link.addEventListener('click', titleClickHandler);
     }
+
+    function generateTags() {
+    
+        /* find all articles */
+
+        const articles = document.querySelectorAll(optArticleSelector);
+        console.log(articles);
+
+        /* START LOOP: for every article: */
+        for (let article of articles) {
+        
+        /* find tags wrapper */
+            const tagList = article.querySelector(optArticleTagSelector);
+            console.log('stała tagList:', optArticleTagSelector);
+        }
+        /* make html variable with empty string */
+
+            let html = '';
+            
+        /* get tags from data-tags attribute */
+
+        /* split tags into array */
+
+        /* START LOOP: for each tag */
+
+        /* generate HTML of the link */
+
+        /* add generated code to html variable */
+
+        /* END LOOP: for each tag */
+
+        /* insert HTML of all the links into the tags wrapper */
+
+        /* END LOOP: for every article: */
+        }   
+
+    generateTags();
+
+
+
 }
-
-function generateTags() {
-    /* find all articles */
-
-    /* START LOOP: for every article: */
-
-    /* find tags wrapper */
-
-    /* make html variable with empty string */
-
-    /* get tags from data-tags attribute */
-
-    /* split tags into array */
-
-    /* START LOOP: for each tag */
-
-    /* generate HTML of the link */
-
-    /* add generated code to html variable */
-
-    /* END LOOP: for each tag */
-
-    /* insert HTML of all the links into the tags wrapper */
-
-    /* END LOOP: for every article: */
-}
-
-generateTags();
